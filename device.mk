18cfe0858a0546e73fe68785e2c9a5a8532a8b12
TheStrix <parthbhatia98@gmail.com>
2019-01-08 14:40:26 +0530

    whyred: Inherit perf blobs from our vendor/qcom/common

--- device.mk
+++ device.mk
@@ -322,6 +322,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/qti_whitelist.xml
 
 
+# Perf
+-include vendor/qcom/common/qti-vendor.mk
+
 # QMI
 PRODUCT_PACKAGES += \
     libjson
